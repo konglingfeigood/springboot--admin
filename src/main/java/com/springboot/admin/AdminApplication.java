d544ab12a12dcb04cfc55c1e3485426070b98389
konglingfei <konglingfei@kingsoft.com>
2019-12-10 10:17:35 +0800

    springboot admin 初始化  孔  12.09

---
+++ src/main/java/com/springboot/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.springboot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @ClassName AdminApplication
+ * @Author こうれいひ
+ * @date 2019/12/10 10:15
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
